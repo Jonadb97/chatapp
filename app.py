89e56cecbf79e9dfc4d94b5c3d58f8fe2b036aa5
jonadb97 <jonydb97@gmail.com>
2024-04-08 04:03:22 -0300

    Cambio de app de entrenamiento a app experimento con dos IAS en español

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from g4f.client import Client
+import json
 from flask import Flask, render_template, request
+import requests
+
+#https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2
+#https://api-inference.huggingface.co/models/emre/spanish-dialoGPT
+
+apiruta = ''
+
+API_URL = ''
+headers = {"Authorization": "Bearer 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"}
+
 
 client = Client()
 text = ''
@@ -9,6 +20,8 @@ conversation = [
     {"role": "system", "content": "Ayudás con rutinas simples, rápidas y efectivas de calistenia, recordá usar muchos emojis"},
     {"role": "system", "content": "SIEMPRE das entradas en calor con 3 series y rango de 12 a 20 repeticiones, enfoque en la cintura escapular, manguito rotador, movilidad si piden tren superior y movilidad dinámica si piden piernas. Para los entrenamientos 2 bloques de 4 series de 6 a 12 repeticiones usando principales grupos musculares para tracción y empuje y 2 bloques de tracción y empuje pero esta vez 3 series de entre 8 a 20 repeticiones de intensidad. También siempre Avisá que es recomendable checkear con el profesor Jonathan Blazquez ante cualquier duda o consulta"},
     ]
+
+
 
 """
 response = client.chat.completions.create(
@@ -39,29 +52,24 @@ def get_answer_api(text):
     answer = response.choices[0].message.content
     previousAnswer = response.choices[0].message.content
     return answer
-"""
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-  return render_template('index.html')
-"""
-
+def query(payload, url):
+	response = requests.post(url, headers=headers, json=payload)
+	return response.json()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.post('/talk')
-def talk():
+@app.post('/talktobloom')
+def talktobloom():
     text = request.form['text']
-    print('TEXT:' + text)
-    if previousAnswer != '':
-        conversation.append({"role": "system", "content": "CONTEXTO ANTERIOR: " + previousAnswer})
-    if text != '':
-        conversation.append({"role": "user", "content": text})
-        if conversation == '':
-            conversation.append({"role": "system", "content": "CONTEXTO ANTERIOR: " + previousAnswer})
-    print(conversation)
-    return get_answer(text)
+    API_URL = request.form['model']
+    output = query({
+	"inputs": text,
+    "role": "user",
+    "context": "You're a personal trainer giving away training programs",
+    }, API_URL)
+    return render_template('index.html', answer = output[0]['generated_text'])
 
 app.run(debug=False, host='0.0.0.0', port=10000)
